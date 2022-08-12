748b576858124b0ad0bcd5f6f9947a81cfde02ab
Winstone <Winstone1108@naver.com>
2022-08-12 10:19:16 +0900

    Chapter 10

--- C_Edu_10/C_Edu_10/Main.cpp
+++ C_Edu_10/C_Edu_10/Main.cpp
@@ -10,8 +10,10 @@ int main(void)
 {
 	// ** 배열과 포인터의 관계
 	{
+		// ** 배열 선언 및 초기화
 		int Array[8] = { 0 };
 
+		// ** 초기화
 		for (int i = 0; i < 8; ++i)
 			Array[i] = i + 1;
 
@@ -19,8 +21,6 @@ int main(void)
 		for (int i = 0; i < 8; ++i)
 			printf("%d\n", Array[i]);
 		printf("\n");
-
-
 
 
 		printf("[포인터를 사용한 출력]\n");
@@ -46,6 +46,80 @@ int main(void)
 
 		printf("\n");
 	}
+
+
+
+	// ** 로또 번호를 생성하는 프로그램을 작성해 봅시다.
+	/*
+	* 참고: 1등 확률 : 1/8155060
+	*/
+	/*
+	int Number[5][6] = {
+		{0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0}, 
+		{0, 0, 0, 0, 0, 0}, 
+		{0, 0, 0, 0, 0, 0}, 
+		{0, 0, 0, 0, 0, 0}
+	};
+	*/
+	int Number[5][6];
+
+	for (int i = 0; i < 5; ++i)
+	{
+		for (int j = 0; j < 6; ++j)
+		{
+			srand((i+1) * (j + 1) * rand());
+			Number[i][j] = rand() % 45 + 1;
+		}
+	}
+
+	// ** 버블 정렬
+	/*
+	for (int i = 0; i < 5; ++i)
+	{
+		for (int j = 0; j < 5; ++j)
+		{
+			for (int k = (j + 1); k < 6; ++k)
+			{
+				if (Number[i][j] > Number[i][k])
+				{
+					int Temp = Number[i][j];
+					Number[i][j] = Number[i][k];
+					Number[i][k] = Temp;
+				}
+			}
+		}
+	}
+	*/
+
+	// ** 선택 정렬
+	for (int i = 0; i < 5; ++i)
+	{
+		for (int j = 0; j < 5; ++j)
+		{
+			int Select = j;
+			
+			for (int k = (j + 1); k < 6; ++k)
+			{
+				if (Number[i][Select] > Number[i][k])
+					Select = k;
+			}
+
+			int Temp = Number[i][j];
+			Number[i][j] = Number[i][Select];
+			Number[i][Select] = Temp;
+		}
+	}
+
+	for (int i = 0; i < 5; ++i)
+	{
+		printf("%02d. ", i + 1);
+		for (int j = 0; j < 6; ++j)
+			printf("%02d ", Number[i][j]);
+		printf("\n");
+	}
+		
+
 
 	return 0;
 }
