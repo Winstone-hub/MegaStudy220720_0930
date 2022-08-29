063cbf200adfb58f031bb88e1a7f59157fa53804
Winstone <Winstone1108@naver.com>
2022-08-29 09:39:39 +0900

    Start

--- C+_Edu_04/C+_Edu_04/Main.cpp
+++ C+_Edu_04/C+_Edu_04/Main.cpp
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <Windows.h>
 
-
 // ** 충돌처리.
 // ** 생성자 & 복사생성자.
 // ** 장면 관리 (scene 전환)
 // ** 생성 함수.
 // ** 함수 정리.
-
 
 struct Vector2
 {
@@ -89,76 +87,6 @@ int main(void)
 				Player.Position.y);
 
 			printf("%s", Player.Texture);
-
-
-
-			/*
-			* 총알 생성.
-			* 2.5초 간격으로 총알을 생성하고, 생성 하는 과정에서 현재 비활성화 되어있는 총알을 찾아 좌표를 수정하고,
-			* 활성화 상태로 변경한다. 총알을 1발만 생성 할것이므로 활성화로 변경이 완료되면 즉시 반복문을 탈줄한다.
-			*/
-			for (int i = 0; i < 128; i++)
-			{
-				// ** 총알이 2.5초 단위로 생성됨.
-				if (BulletDelay + 2500 < GetTickCount64())
-				{
-					//if (ShowBullet[i] == false)
-					// ** 총알이 비활성화 상태라면...
-					if (!ShowBullet[i])
-					{
-						// ** 재생성 되었다면 좌표를 다시 초기화 해준다.
-						Bullet[i].Position.x = 118;
-						Bullet[i].Position.y = rand() % 40;
-
-						// ** 현재 상태를 활성화 상태로 변경 한다.
-						ShowBullet[i] = true;
-
-						// ** 시간값을 재 초기화 한다.
-						BulletDelay = GetTickCount64();
-
-						// ** 총알을 1개만 활성화 할 것이기 때문에
-						// ** 반복문을 빠저나간다.
-						break;
-					}
-				}
-			}
-
-
-			// ** 활성화 중인 총알을 그린다.
-			for (int i = 0; i < 128; i++)
-			{
-				//if (ShowBullet[i] == true)
-				// ** 총알이 활성화 상태라면...
-				if (ShowBullet[i])
-				{
-					// ** 총알의 이동. 
-					// ** 이동방향 :  ←←←
-					Bullet[i].Position.x--;
-
-					// ** 충돌판정.
-					if (Bullet[i].Position.x <= 0)
-					{
-						ShowBullet[i] = false;
-						continue;
-					}
-
-					// ** 크기값을 함께 설정해서 범위 충돌 만들어야 함.
-					if (Bullet[i].Position.x == Player.Position.x &&
-						Bullet[i].Position.y == Player.Position.y)
-					{
-						ShowBullet[i] = false;
-						continue;
-					}
-
-					// ** 총알이 제 위치에 그려질수 있도록 커서를 이동시킨다.
-					SetCursorPosition(
-						Bullet[i].Position.x,
-						Bullet[i].Position.y);
-
-					// ** 총알을 그린다.
-					printf("%s", Bullet[i].Texture);
-				}
-			}
 		}
 	}
 
