841670ea4233cc928690a849863784f7323b4d90
Winstone <Winstone1108@naver.com>
2022-08-31 10:59:31 +0900

    function

--- C+_Edu_06/C+_Edu_06/Main.cpp
+++ C+_Edu_06/C+_Edu_06/Main.cpp
@@ -1,23 +1,31 @@
 #include "Headers.h"
 
-
-// ** 장면 관리 (scene 전환)
-
-// ** 생성 함수.
 // ** 함수 정리.
+// ** 키 입력 함수 수정.
 
-
-bool Horzontal = false;
-bool Vertical = false;
+// ** 장면전환을 하기위한 변수
+// ** 현재 장면을 보관한다.
 SceneID SceneState = SceneID::LOGO;
 
 
 void SetScene(Object* _pPlayer, Object* _pEnemy);
 Object* CreateObject(int _x, int _y, char* _Texture);
 void SetCursorPosition(int _x, int _y);
-void Render(char* _str, int _x, int _y);
 void ShowCursor(bool _b);
 void InputKey(Object* _Object);
+
+void LogoProgress();
+void LogoRender(char* _str, int _x, int _y);
+
+void MenuProgress();
+void MenuRender(char* _str, int _x, int _y);
+
+void StageProgress(Object* _Player, Object* _Enemy);
+void StageRender(Object* _Player, Object* _Enemy);
+bool Collision(Object* _Temp, Object* _Dest);
+
+
+
 
 
 int main(void)
@@ -30,20 +38,17 @@ int main(void)
 	Object* pPlayer = CreateObject(int(120 * 0.3333f), 40 >> 1, (char*)"△");
 
 	// ** Enemy 초기화
-	Object Enemy;
-	Enemy.Position = Vector2(int(120 * 0.3333f * 2), 40 >> 1);
-	Enemy.Texture = (char*)"■";
-	Enemy.Scale = Vector2((int)strlen(Enemy.Texture), 1);
+	Object* Enemy = CreateObject(int(120 * 0.3333f * 2), 40 >> 1, (char*)"■");
 
 	while (true)
 	{
-		if (Time + 50 < GetTickCount64())
+		if (Time + 50 <= GetTickCount64())
 		{
 			Time = GetTickCount64();
 
 			system("cls");
 
-			SetScene(pPlayer, &Enemy);
+			SetScene(pPlayer, Enemy);
 		}
 	}
 
@@ -56,16 +61,14 @@ void SetScene(Object* _pPlayer, Object* _pEnemy)
 	switch (SceneState)
 	{
 	case SceneID::LOGO:
-		Render((char*)"LOGO", 58, 20);
-
-		if (GetAsyncKeyState(VK_RETURN) || GetAsyncKeyState(VK_ESCAPE))
-			SceneState = SceneID::MENU;
+		LogoProgress();
+		LogoRender((char*)"Logo", 58, 20);
 		break;
 	case SceneID::MENU:
-		Render((char*)"MENU", 58, 20);
+		MenuProgress();
+		MenuRender((char*)"MENU", 58, 20);
 
-		if (GetAsyncKeyState('S'))
-			SceneState = SceneID::STAGE;
+		
 		break;
 	case SceneID::STAGE:
 		// ** ===== Progress ===== 
@@ -82,14 +85,7 @@ void SetScene(Object* _pPlayer, Object* _pEnemy)
 			_pEnemy->Position.x,
 			_pEnemy->Position.y);
 
-		// ** 충돌
-		if (_pPlayer->Position.x + 2 > _pEnemy->Position.x &&
-			_pEnemy->Position.x + 2 > _pPlayer->Position.x &&
-			_pPlayer->Position.y == _pEnemy->Position.y)
-		{
-			SetCursorPosition(120 >> 1, 1);
-			printf("충돌 입니다.");
-		}
+		
 
 		if (GetAsyncKeyState('Q'))
 			SceneState = SceneID::EXIT;
@@ -100,18 +96,27 @@ void SetScene(Object* _pPlayer, Object* _pEnemy)
 	}
 }
 
+// ** 오브젝트 생성 함수.
 Object* CreateObject(int _x, int _y, char* _Texture)
 {
+	// ** Object를 동적으로 생성한다.
 	Object* pObj = (Object*)malloc(sizeof(Object));
-	
+
+	// ** 좌표를 셋팅
 	pObj->Position.x = _x;
 	pObj->Position.y = _y;
 
+	// ** 동적할당을 해준다. 동적할당을 할때 문자열의 크기 + 1 만큼의 크기로 해준다.
 	pObj->Texture = (char*)malloc(strlen(_Texture) + 1);
+	
+	// ** 문자열을 복사한다.
 	strcpy(pObj->Texture, _Texture);
 
+	// ** 문자열의 길이로 크기를 정한다.
 	pObj->Scale = Vector2((int)strlen(pObj->Texture), 1);
-	
+
+	// ** 모든 셋팅이 완료되었다면 
+	// ** 동적으로 생성된 Object 의 주소를 반환한다.
 	return pObj;
 }
 
@@ -126,13 +131,6 @@ void SetCursorPosition(int _x, int _y)
 		GetStdHandle(STD_OUTPUT_HANDLE), pos);
 	// ** GetStdHandle(STD_OUTPUT_HANDLE) = 현재 콘솔창의 핸들.
 	// ** HANDLE : 윈도우 창의 기본 설정값을 셋팅할 수 있음.
-}
-
-
-void Render(char* _str, int _x, int _y)
-{
-	SetCursorPosition(_x, _y);
-	printf("%s", _str);
 }
 
 // ** 커서를 보이게/안보이게 하는 함수.
@@ -157,6 +155,9 @@ void ShowCursor(bool _b)
 // ** 플레이어의 키 입력을 받고, 입력에 따라 Texture 를 변경 한다.
 void InputKey(Object* _Object)
 {
+	bool Horzontal = false;
+	bool Vertical = false;
+
 	if (GetAsyncKeyState(VK_UP) && !Horzontal)
 	{
 		if (_Object->Position.y > 0)
@@ -200,4 +201,60 @@ void InputKey(Object* _Object)
 	}
 	else
 		Horzontal = false;
+}
+
+
+
+void LogoProgress()
+{
+	if (GetAsyncKeyState(VK_RETURN) || GetAsyncKeyState(VK_ESCAPE))
+		SceneState = SceneID::MENU;
+}
+
+void LogoRender(char* _str, int _x, int _y)
+{
+	SetCursorPosition(_x, _y);
+	printf("%s", _str);
+}
+
+
+void MenuProgress()
+{
+	if (GetAsyncKeyState('S'))
+		SceneState = SceneID::STAGE;
+}
+
+void MenuRender(char* _str, int _x, int _y)
+{
+	SetCursorPosition(_x, _y);
+	printf("%s", _str);
+}
+
+
+void StageProgress(Object* _Player, Object* _Enemy)
+{
+	if (Collision(_Player, _Enemy))
+	{
+		SetCursorPosition(120 >> 1, 1);
+		printf("충돌 입니다.");
+	}
+}
+
+void StageRender(Object* _Player, Object* _Enemy)
+{
+	SetCursorPosition(_Player->Position.x, _Player->Position.y);
+	printf("%s", _Player->Texture);
+
+	SetCursorPosition(_Enemy->Position.x, _Enemy->Position.y);
+	printf("%s", _Enemy->Texture);
+}
+
+// ** 충돌처리 함수
+bool Collision(Object* _Temp, Object* _Dest)
+{
+	if (_Temp->Position.x + _Temp->Scale.x > _Dest->Position.x &&
+		_Dest->Position.x + _Dest->Scale.x > _Temp->Position.x &&
+		_Temp->Position.y == _Dest->Position.y)
+		return true;
+	return false;
 }
