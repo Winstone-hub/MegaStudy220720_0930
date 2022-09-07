855dada07c1fc920d5674807fa4e989e57115bdf
Winstone <Winstone1108@naver.com>
2022-09-07 11:17:09 +0900

    NodeList

--- C+_Edu_11_List/C+_Edu_11_List/Main.cpp
+++ C+_Edu_11_List/C+_Edu_11_List/Main.cpp
@@ -1,31 +1,51 @@
 #include <stdio.h>
 #include <malloc.h>
 
-
 struct Node
 {
 	int Value;
 	Node* Next;
 };
 
+Node* Begin;
+Node* End;
 
+void AddData(int _Value);
+void Output();
 
 int main(void)
 {
 	Node* List = (Node*)malloc(sizeof(Node));
+	Begin = List;
+	End = List;
 	List->Value = 10;
 
-	List->Next = (Node*)malloc(sizeof(Node));
-	List->Next->Value = 20;
+	AddData(20);
+	AddData(30);
+	AddData(40);
+	AddData(50);
 
-	List->Next->Next = (Node*)malloc(sizeof(Node));
-	List->Next->Next->Value = 30;
-
-
-	printf("%d\n", List->Value);
-	printf("%d\n", List->Next->Value);
-	printf("%d\n", List->Next->Next->Value);
-
+	Output();
 
 	return 0;
+}
+
+void AddData(int _Value)
+{
+	End->Next = (Node*)malloc(sizeof(Node));
+	End = End->Next;
+
+	End->Next = nullptr;
+	End->Value = _Value;
+}
+
+void Output()
+{
+	Node* NodeList = Begin;
+
+	while (NodeList != nullptr)
+	{
+		printf("%d\n", NodeList->Value);
+		NodeList = NodeList->Next;
+	}
 }
