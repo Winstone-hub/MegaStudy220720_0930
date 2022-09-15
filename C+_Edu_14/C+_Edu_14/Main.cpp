eac4ae153e6779bde30dceb50769e9a69a920679
Winstone <Winstone1108@naver.com>
2022-09-15 11:21:09 +0900

    vector

--- C+_Edu_14/C+_Edu_14/Main.cpp
+++ C+_Edu_14/C+_Edu_14/Main.cpp
@@ -17,9 +17,13 @@ int main(void)
 {
 	push_back(10);
 	push_back(20);
+	push_back(30);
+	push_back(40);
+	push_back(50);
 
 	printf("%d\n", *Fornt);
 	printf("%d\n", *Back);
+	printf("%d\n", size);
 
 	/*
 	for (int i = 0; i < 10; ++i)
@@ -39,8 +43,22 @@ int main(void)
 
 void push_back(int _value)
 {
+	int* Temp = (int*)malloc(sizeof(int) * size);
+
+	for (int i = 0; i < size; ++i)
+		Temp[i] = Vector[i];
+
+	free(Vector);
+
 	++size;
+
 	Vector = (int*)malloc(sizeof(int) * size);
+
+	for (int i = 0; i < size - 1; ++i)
+		Vector[i] = Temp[i];
+
+	free(Temp);
+
 	Vector[size - 1] = _value;
 
 	Fornt = &Vector[0];
