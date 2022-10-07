200c5cab8d7240cd8ec21707541f650f1de3e4b0
Winstone <Winstone1108@naver.com>
2022-10-07 11:17:39 +0900

    operator

---
+++ C++_Edu_06/C++_Edu_06/Child.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "Parent.h"
+
+class Child : public Parent
+{
+public:
+	// ** 오버라이딩 : 상속관계에서만 사용가능
+	// ** 똑같은 이름의 함수를 사용할 때 어떤함수를 호출해야할지 모호한경우.
+	// ** 함수를 호출하는 기술.
+
+	virtual void Start()override 
+	{
+		// ** 오버라이드 함수.
+	}
+	virtual void Start(int number)override
+	{
+
+	}
+
+	// ** 오버로딩 : 상속관계랑은 전혀 상관이 없음. 
+	// ** 같은 클래스 내부에서 사용되며, 똑같은 이름의 함수를 호출하는 기술.
+	// ** 매개변수의 개수나 형태에 따라 호출되는 함수가 달라진다.
+	void Update(int)
+	{
+		// ** 오버로드 함수.
+	}
+
+	void Update(float)
+	{
+		// ** 오버로드 함수.
+	}
+
+	void Update()
+	{
+		// ** 오버로드 함수.
+	}
+};
+
---
+++ C++_Edu_06/C++_Edu_06/Parent.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Headers.h"
+
+class Parent
+{
+protected:
+	string Name;
+	
+	// ** [추상 클래스 특징]
+	// ** 데이터는 존재할 수 있지만, 함수는 순수 가상 함수로만 존재할 수 있다.
+	
+	// ** [순수가상함수의 특징]
+	// ** 순수가상함수가 1개라도 만들어지면, 그 클래스는 추상 클래로만 사용 가능하다.
+
+	// ** [정리]
+	// ** 순수가상함수를 1개라도 만들면, 일반함수 제작 및 사용 불가.
+
+public:
+	// ** 순수 가상함수.
+	virtual void Start()PURE;
+	virtual void Start(int number)PURE;
+};
+
+
+
